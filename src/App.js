b51278e96bdf0957177d68e411fcd8371f698b52
Omar Monib <omar.monib.03@gmail.com>
2024-11-13 22:02:34 +0200

    Add App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Form, Input } from "antd";
+import { IdcardTwoTone } from "@ant-design/icons";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const onFinish = (values) => console.log("Form values:", values);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        name="login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <div className="login">
+          <div className="logo">
+            <IdcardTwoTone className="icon" />
+            <p>LOGIN FORM</p>
+          </div>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Sign in
+            </Button>
+          </Form.Item>
+        </div>
+      </Form>
     </div>
   );
-}
+};
 
 export default App;
